df6d25e8e349b5c97d935810e052d3edd9677b8e
Maksim <mlaskov73@gmail.com>
2024-05-28 14:29:35 +0300

    practic js №1

--- scr.js
+++ scr.js
@@ -48,3 +48,23 @@
 // console.log(dd['b']);
 
 
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+
+const personalMovieDB = {
+    count: 'numberOfFilms',
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const film = prompt('Один из последних просмотренных фильмов?');
+const review = prompt('На сколько оцените его?');
+const film2 = prompt('Один из последних просмотренных фильмов?');
+const review2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[film] = review;
+personalMovieDB.movies[film2] = review2;
+
+console.log(personalMovieDB);
